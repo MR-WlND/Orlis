cd3f51f050a6a67cf8c04d8ee7d8c98a7f6ab801
theanishtar <long16730z@gmail.com>
2026-09-10 02:02:20 +0700

    fix: force https scheme for production to avoid mixed content block

--- Orlis/app/Providers/AppServiceProvider.php
+++ Orlis/app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         Order::observe(OrderObserver::class);
         Product::observe(ProductObserver::class);
 
